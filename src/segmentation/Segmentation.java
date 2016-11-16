8300d10749968d526b0b141ff3676e67176aefd3
BaptisteWolff <baptist21@live.fr>
2016-11-16 16:21:38 +0100

    Afficher la segmentatiion sur l'image test.

--- src/segmentation/Segmentation.java
+++ src/segmentation/Segmentation.java
@@ -41,45 +41,83 @@ public class Segmentation {
 		Imgproc.threshold(mask, mask, 215, 255, Imgproc.THRESH_TOZERO_INV);
 
 		// morphological operators
-		// dilate with large element, erode with small ones
-		int r=9;
-		
-		//Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(r, r));
-		//Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(r, r));
-		
+
+		// On fait une ouverture pour supprimer les parasites plus petits que la
+		// balle.
+
+		int r = 9;
+
 		Mat erodeElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(r, r));
 		Mat dilateElement = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(r, r));
-		
+
 		Imgproc.erode(mask, morphOutput, erodeElement);
 		Imgproc.dilate(morphOutput, morphOutput, dilateElement);
-		
-		int r2=10;
-		
-		//Mat erodeElement2 = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(r2, r2));
-		//Mat dilateElement2 = Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(r2, r2));
+
+		// On fait une ouverture pour supprimer la balle.
+		int r2 = 10;
+
+		// Mat erodeElement2 =
+		// Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(r2,
+		// r2));
+		// Mat dilateElement2 =
+		// Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(r2,
+		// r2));
 
 		Mat erodeElement2 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(r2, r2));
 		Mat dilateElement2 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(r2, r2));
 
-		
 		Imgproc.erode(mask, morphOutput2, erodeElement2);
 		Imgproc.dilate(morphOutput2, morphOutput2, dilateElement2);
-		
-		
+
+		// on veut récupérer la balle mais il reste des élément de la même
+		// taille que celle ci.
 		Core.subtract(morphOutput, morphOutput2, morphOutput3);
-		
-		Mat erodeElement3 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(r2/2, 1.2*r2));
-		Mat dilateElement3 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(r2/2, 1.2*r2));
+
+		Mat erodeElement3 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(r2 / 2, 1.2 * r2));
+		Mat dilateElement3 = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(r2 / 2, 1.2 * r2));
 
 		Imgproc.dilate(morphOutput3, morphOutput3, dilateElement3);
 		Imgproc.erode(morphOutput3, morphOutput3, erodeElement3);
 		Imgproc.erode(morphOutput3, morphOutput4, erodeElement3);
 		Imgproc.dilate(morphOutput4, morphOutput4, dilateElement3);
-		
+
 		Core.subtract(morphOutput3, morphOutput4, morphOutput5);
-		
+
 		Imgproc.erode(morphOutput5, morphOutput5, erodeElement);
 		Imgproc.dilate(morphOutput5, morphOutput5, dilateElement);
+
+		int sommeX = 0;
+		int sommeY = 0;
+		int nbPixels = 0;
+
+		System.out.println("x = " + morphOutput5.get(1, 1)[1]);
+
+		for (int x = 0; x < morphOutput5.width(); x++) {
+			for (int y = 0; y < morphOutput5.height(); y++) {
+
+				if (morphOutput5.get(y, x)[1] != 0.0) {
+					sommeX += x;
+					sommeY += y;
+					nbPixels++;
+					//Modifier l'image originale
+					double[] data = frame.get(y, x);
+			        data[0] = 0;
+			        data[1] = 0;
+			        data[2] = 0;
+			        frame.put(y, x, data);
+				}
+			}
+		}
+		
+		if (nbPixels==0){
+			nbPixels=1;
+		}
+		double X = sommeX / nbPixels;
+		double Y = sommeY / nbPixels;
+		
+
+		System.out.println("x = " + X + "; y = " + Y);
+		
 		
 		// Test
 		Highgui.imwrite("D:/Users/Baptiste/Pictures/BigMaccadam/test1.jpg", blurredImage);
@@ -90,7 +128,7 @@ public class Segmentation {
 		Highgui.imwrite("D:/Users/Baptiste/Pictures/BigMaccadam/test6.jpg", morphOutput3);
 		Highgui.imwrite("D:/Users/Baptiste/Pictures/BigMaccadam/test7.jpg", morphOutput4);
 		Highgui.imwrite("D:/Users/Baptiste/Pictures/BigMaccadam/test8.jpg", morphOutput5);
+		Highgui.imwrite("D:/Users/Baptiste/Pictures/BigMaccadam/test9.jpg", frame);
 	}
-	
 
 }
